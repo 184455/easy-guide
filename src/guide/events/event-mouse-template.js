0dcf7f2d1bb631d1d7d03eb37c11e12e22439c78
19055 <zhongji.huang@hand-china.com>
2021-01-02 10:01:49 +0800

    feat: 调整操作栏

--- src/guide/events/event-mouse-template.js
+++ src/guide/events/event-mouse-template.js
@@ -3,15 +3,17 @@ import { tagX, tagY } from '../../config/constant'
 import { setStyles } from '../../utils/dom'
 
 export function handleTemplateDown(_this, event) {
-  const { offsetLeft, offsetTop } = _this.currentTarget.parentElement // 从父元素取距离屏幕的位置
+  const rootEle = document.getElementById('_eG_template')
+  const { offsetLeft, offsetTop } = rootEle // 从父元素取距离屏幕的位置
   _this.onMouseDownPositionImage = {
     deltaX: event[tagX] - offsetLeft,
-    deltaY: event[tagY] - offsetTop
+    deltaY: event[tagY] - offsetTop,
+    rootEle
   }
 }
 export function handleTemplateMove(_this, event) {
-  const { deltaX, deltaY } = _this.onMouseDownPositionImage // 鼠标落点和元素的边距，需要减去，保持移动前不抖动
-  setStyles(_this.currentTarget.parentElement, {
+  const { deltaX, deltaY, rootEle } = _this.onMouseDownPositionImage // 鼠标落点和元素的边距，需要减去，保持移动前不抖动
+  setStyles(rootEle, {
     left: `${event[tagX] - deltaX}px`,
     top: `${event[tagY] - deltaY}px`
   })
