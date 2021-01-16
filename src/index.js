29d30708beabe0b7a395b81fc9f50aeb6882e4d0
19055 <zhongji.huang@hand-china.com>
2021-01-16 21:19:21 +0800

    feat: 添加 0.0.6 版本

--- src/index.js
+++ src/index.js
@@ -11,6 +11,6 @@ InitDataInterface(EasyGuide)
 InitEvents(EasyGuide)
 GuideEditModal(EasyGuide)
 
-EasyGuide.prototype.Version = '0.0.5'
+EasyGuide.prototype.Version = '0.0.6'
 
 export default EasyGuide
